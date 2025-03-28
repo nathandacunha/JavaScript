53011771e8e7e7a16a28ff444f3909c62683c033
nathandacunha <nathan_cunha@estudante.sesisenai.org.br>
2025-03-28 11:59:46 -0300

    criacao dos arquivos basicos

---
+++ Atividade 7/exercicio07.js
@@ -0,0 +1,13 @@
+/*
+Faça um algoritmo para uma picoleteria que ao selecionar o tipo de sabor o preço do picolé é mostrado, os sabores:
+a. Chocolate custa R$:1,50
+b. Morango custa R$:2,50
+c. Creme custa R$:2,50
+d. Manga custa R$:3,20
+e. Melancia custa R$:3,40
+f. Vanilla Ice custa R$:3,00
+g. Céu Azul custa R$:3,60
+h. Brownie custa R$:4,00
+i. Hawaiano custa R$:5,00
+Obs.: Neste exercício também é utilizado array.
+*/ 
