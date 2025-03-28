55b44796c312ba14f555d7dfdcacdbea6b61db50
nathandacunha <nathan_cunha@estudante.sesisenai.org.br>
2025-03-28 10:07:03 -0300

    utilizacao includes() no js

--- Atividade 6/exercicio06.js
+++ Atividade 6/exercicio06.js
@@ -1,8 +1,9 @@
 // 6. Faça um algoritmo que verifique a letra digitada e imprima se ela é uma vogal ou consoante. (Dica: pesquise sobre o uso de array).
 
 function verificacaoLetra() {
-    const vogal = ["a", "e", "i", "o", "u"];
     letraDigitar = prompt("Digite uma letra: ");
 
-    
+    if (letraDigitar.includes("a", "i", "o", "u")) {
+        alert("A letra é vogal");
+    }
 }
