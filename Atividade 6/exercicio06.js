28bbe7232c254aa389f5a7b798762584229c1e0f
nathandacunha <nathan_cunha@estudante.sesisenai.org.br>
2025-03-28 10:03:59 -0300

    Criacao dos arquivos e da funcao no js com o arrays

---
+++ Atividade 6/exercicio06.js
@@ -0,0 +1,8 @@
+// 6. Faça um algoritmo que verifique a letra digitada e imprima se ela é uma vogal ou consoante. (Dica: pesquise sobre o uso de array).
+
+function verificacaoLetra() {
+    const vogal = ["a", "e", "i", "o", "u"];
+    letraDigitar = prompt("Digite uma letra: ");
+
+    
+}
