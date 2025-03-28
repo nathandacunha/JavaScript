90373e86509318ab77ddf0ff619e14f0599adc84
nathandacunha <nathan_cunha@estudante.sesisenai.org.br>
2025-03-28 09:37:07 -0300

    utilizando o sort() no exercicio4

--- Atividade 4/exercicio04.js
+++ Atividade 4/exercicio04.js
@@ -1,7 +1,7 @@
 // 4. Faça um algoritmo que peça ao usuário 4 valores inteiros e mostre-os em ordem decrescente. 
 
 function ordemDecrescente(){
-    const numeros = [];
+    var numeros = [];
     num1 = parseInt(prompt("Digite um número inteiro: "));
     numeros[0] = num1;
     num2 = parseInt(prompt("Digite mais número inteiro: "));
@@ -11,4 +11,8 @@ function ordemDecrescente(){
     num4 = parseInt(prompt("Digite mais número inteiro: "));
     numeros[3] = num4;
  
+    var numeros = [num1, num2, num3, num4];
+    numeros.sort();
+    numeros.reverse();
+
 }
