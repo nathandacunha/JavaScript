066dc2cd3594a9b79cbe32c30e8e7306d62265be
nathandacunha <nathan_cunha@estudante.sesisenai.org.br>
2025-03-31 11:50:25 -0300

    atualizacao de variavel

--- Atividade 6/exercicio06.js
+++ Atividade 6/exercicio06.js
@@ -1,9 +1,9 @@
 // 6. Faça um algoritmo que verifique a letra digitada e imprima se ela é uma vogal ou consoante. (Dica: pesquise sobre o uso de array).
 
 function verificacaoLetra() {
-    letraDigitar = prompt("Digite uma letra: ");
+    let letraDigitar = prompt("Digite uma letra: ");
 
-    letraVogais = ["a", "e", "i", "o", "u"];
+    let letraVogais = ["a", "e", "i", "o", "u"];
 
     if (Array.prototype.indexOf(letraDigitar.toLowerCase()) != 0 ) {
         alert("Vogal");
