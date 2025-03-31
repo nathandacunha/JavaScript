75bb67e6ee90a829e8f8f95c67e9d1b971a4abb2
nathandacunha <nathan_cunha@estudante.sesisenai.org.br>
2025-03-31 11:48:04 -0300

    atualizacao de variavel

--- Atividade 5/exercicio05.js
+++ Atividade 5/exercicio05.js
@@ -1,6 +1,6 @@
 // 5. Faça um algoritmo que peça um número e se este número for par, transforme-o em ímpar e vice-versa. 
 function transformacaoNumeroImpar() {
-    num = parseFloat(prompt("Digite um número: "));
+    let num = parseFloat(prompt("Digite um número: "));
     numeroPar = num/2;
     if (numeroPar == 0) {
         num = num+1;
