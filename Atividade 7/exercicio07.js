c7886c1e65e688f53c7173b0f9b237240e3c10b1
nathandacunha <nathan_cunha@estudante.sesisenai.org.br>
2025-03-31 11:37:12 -0300

    feito a funcao do js com estrutura de decisao

--- Atividade 7/exercicio07.js
+++ Atividade 7/exercicio07.js
@@ -11,3 +11,36 @@ h. Brownie custa R$:4,00
 i. Hawaiano custa R$:5,00
 Obs.: Neste exercício também é utilizado array.
 */ 
+
+function precoPicole(){
+    alert("a. Chocolate")
+    alert("b. Morango")
+    alert("c. Creme")
+    alert("d. Manga")
+    alert("e. Melancia")
+    alert("f. Vanilla Ice")
+    alert("g. Céu Azul")
+    alert("h. Brownie")
+    alert("i. Hawaiano")
+
+    opcoes = prompt("Decide um tipo de picole (tudo minusculo): \n");
+    if (opcoes == "a"){
+        alert("Preço:s R$1.50"); //1.50
+    } else if (opcoes == "b") {
+        alert("Preço R$2,50") // 2,50
+    } else if (opcoes == "c") {
+        alert("Preço: R$2,50"); // 2,50
+    } else if (opcoes == "d"){
+        alert("Custa: R$3,20") // 3,20
+    } else if (opcoes == "e") {
+        alert("Custa: R$3,40") // 3,40
+    } else if (opcoes == "f"){
+        alert("Custa: R$3,00") // 3,00
+    } else if (opcoes == "g") {
+        alert("Custo: R$3,60"); // 3,60
+    } else if (opcoes == "h") {
+        alert("Custa: R$4,00"); // 4,00
+    } else if (opcoes == "i") {
+        alert("Custa: R$5,00"); // 5
+    }
+}
